f5f590ddca6a6d4e6842cbef6249de27d1b10db8
Heriyanto <buibui_chen@yahoo.com>
2022-07-14 11:56:29 +0930

    PetsItemDatabase setup

--- app/src/main/java/com/knowingyourpet/PetsResponseItem.kt
+++ app/src/main/java/com/knowingyourpet/PetsResponseItem.kt
@@ -8,18 +8,19 @@ import androidx.room.PrimaryKey
 )
 data class PetsResponseItem(
     @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val bred_for: String,
     val breed_group: String,
     val country_code: String,
     val description: String,
-    val height: Height,
+  //  val height: Height,
     val history: String,
-    val id: Int? = null,
-    val image: Image,
+
+   // val image: Image,
     val life_span: String,
     val name: String,
     val origin: String,
     val reference_image_id: String,
     val temperament: String,
-    val weight: Weight
+   // val weight: Weight
 )
--- app/src/main/java/com/knowingyourpet/db/PetsItemDatabase.kt
+++ app/src/main/java/com/knowingyourpet/db/PetsItemDatabase.kt
@@ -1,6 +1,8 @@
 package com.knowingyourpet.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.knowingyourpet.PetsResponseItem
 
@@ -12,4 +14,22 @@ import com.knowingyourpet.PetsResponseItem
  abstract class PetsItemDatabase : RoomDatabase(){
 
      abstract fun getPetsItemDao(): PetsDao
+
+     companion object {
+         private var instance: PetsItemDatabase? = null
+         private val LOCK = Any()
+
+         operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+             instance ?: createDatabase(context).also { instance = it }
+         }
+
+         private fun createDatabase(context: Context) =
+             Room.databaseBuilder(
+                 context.applicationContext,
+                 PetsItemDatabase::class.java,
+                 "petsItem_db.db"
+             ).build()
+
+
+     }
 }
